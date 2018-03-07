2fe6bdcf2bcc9df302cc451e787059ce3b9ed1cc
Leigh Broomfield <broomer142@gmail.com>
2018-03-06 21:21:21 -0500

    Update centralized code into functions.

--- GDAX Boilerplate.py
+++ GDAX Boilerplate.py
@@ -9,11 +9,13 @@ Created on Sun Feb 25 00:55:02 2018
 import requests
 import base64, hashlib, hmac, time
 from requests.auth import AuthBase
+import pandas as pd
 # sandbox api base
 sandbox_api_base = 'https://api-public.sandbox.gdax.com'
-my_api_key = 
-passphrase = 
-secret_key = 
+my_api_key = ''
+passphrase = ''
+secret_key = ''
+historicCsvPath = ''
 #insert values here before running code
 
 def products():
@@ -47,12 +49,28 @@ class CoinbaseExchangeAuth(AuthBase):
         return(request)
 
 api_url = 'https://api.gdax.com/'
-ltc_historic_endpoint = '/products/LTC-USD/candles'
-ltc_historic_json = { 'start' : '2018-02-23T00:00:00-05:00', 'end' : '2018-02-24T00:00:00-05:00', 'granularity' : '900'}
 
-auth = CoinbaseExchangeAuth(my_api_key, secret_key, passphrase)
+def coin_csv_create(coin, granularity):
+#coin value must be 'LTC' or 'BTC'
+#The granularity must be one of the following values: {60, 300, 900, 3600, 21600, 86400}. 
+    if coin not in ('LTC', 'BTC'):
+        return(print('Please use LTC or BTC for your coin.'))
+    elif granularity not in ('60', '300', '900', '3600', '21600', '86400'):
+        return(print('Please use 60, 300, 900, 3600, 21600, or 86400 for your granularity.'))
+    historic_endpoint = '/products/{0}-USD/candles'.format('LTC')
+    historic_json = { 'start' : '2018-02-23T00:00:00-05:00', 'end' : '2018-02-24T00:00:00-05:00', 'granularity' : granularity}
 
-response_historic_data = requests.get(api_url + ltc_historic_endpoint,
-                                     json = ltc_historic_json,
-                                     auth=auth)
-print(response_historic_data.json())
+    auth = CoinbaseExchangeAuth(my_api_key, secret_key, passphrase)
+
+    response_historic_data = requests.get(api_url + historic_endpoint,
+                                          json = historic_json,
+                                          auth=auth)
+#    print(response_historic_data.json())
+    historicData = pd.DataFrame(response_historic_data.json())
+    historicData.columns = ['Time', 'Low', 'High', 'Open', 'Close', 'Volume']
+    historicData.to_csv(historicCsvPath)
+    pass
+    
+if __name__ == '__main__':
+    coin_csv_create('LTC', '3600')
+#put code that you want to run after this if statement.
